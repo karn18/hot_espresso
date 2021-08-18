52f000b8eec4f0bfd3ef0b107737d24f1a23bba7
Karn Tirasoontorn <karn@hey.com>
2021-08-18 11:28:56 +0700

    Init

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Configure your import map in config/initializers/importmap.rb
+
+// import "@rails/actioncable"
+// import "@rails/activestorage"
+
+import "@hotwired/stimulus-importmap-autoloader"
+import "@hotwired/turbo-rails"
+
+import 'components/reverse_component'
---
+++ app/javascript/components/reverse_component.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+new Vue({
+  el: '#app-1',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
